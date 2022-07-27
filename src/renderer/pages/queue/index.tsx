b9ed40f46fee7b79102584779b3694bb75fca25d
Kevin Jobs <me@kevinjobs.com>
2022-07-27 10:53:32 +0800

    feat: insert to the top

--- src/renderer/pages/queue/index.tsx
+++ src/renderer/pages/queue/index.tsx
@@ -8,6 +8,7 @@ import {
 import { Track } from "@plugins/player";
 import { useTranslation } from "react-i18next";
 import { IoMdClose } from "react-icons/io";
+import { MdOutlineVerticalAlignTop } from "react-icons/md";
 import debug from "@plugins/debug";
 import Loading from "@components/loading";
 import {player} from "../../app/DataManager";
@@ -40,6 +41,13 @@ export default function PlayQueue() {
     dispatch(setQueue(q));
   }
 
+  const handleInsert = (e: React.MouseEvent<HTMLDivElement>, t: Track) => {
+    const idx = queueIndexOf(queue, t);
+    const q = [...queue];
+    q.splice(idx, 1);
+    dispatch(setQueue([t, ...q.slice(0)]))
+  }
+
   return (
     <div className={"page page-playlist electron-no-drag perfect-scrollbar"}>
       {queue.length ?
@@ -59,13 +67,18 @@ export default function PlayQueue() {
                   idx + 1
                 )}
               </div>
-              <div
-                className={"info"}
-                style={{ color: isCurrent && "#71b15f" }}
-              >
+              <div className={"info"} style={{ color: isCurrent && "#71b15f" }}>
                 <span>{tt.title || t("No Title")}</span>
               </div>
-              <div className={"remove"} onClick={e => handleRemove(e, tt)}>
+              {idx > 0 && (
+                <div
+                  className={"insert-to-top"}
+                  onClick={(e) => handleInsert(e, tt)}
+                >
+                  <MdOutlineVerticalAlignTop size={18} />
+                </div>
+              )}
+              <div className={"remove"} onClick={(e) => handleRemove(e, tt)}>
                 <IoMdClose fill={"#df3636"} size={18} />
               </div>
             </div>
